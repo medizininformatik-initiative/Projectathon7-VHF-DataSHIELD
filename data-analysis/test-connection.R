56a3098c372cb8a5a44ca80ec39ecf416d3d2702
Toralf Kirsten <tkirsten@.uni-leipzig.de>
2023-06-15 17:42:05 +0200

    Extend the test-connection script working on test data

--- data-analysis/test-connection.R
+++ data-analysis/test-connection.R
@@ -3,26 +3,30 @@ rm(list = ls())
 require(DSI)
 require(DSOpal)
 require(opalr)
-
+require(dsBaseClient)
 
 # Connect to the OPAL server
 
 
 # Add login data
 builder <- DSI::newDSLoginBuilder()
-source("./set-login-data.R")
+source("data-analysis/set-login-data.R")
 login.data <- builder$build()
 
 # Login to all OPAL servers
-connections <- DSI::datashield.login(logins = login.data)
+connections <- datashield.login(logins = login.data)
 
 # Load data from patient table and manage it by temporary variable "patient"
-DSI::datashield.assign.table(conns = connections, symbol = "patient", table = "VHF.patient", variables = c("pid", "gender", "age"))
+datashield.assign.table(conns = connections, symbol = "patient", table = "VHF.patient", variables = c("pid", "gender", "age"))
 
+# Calculate the mean over the patient's age
+ds.mean(x = "patient$age", type = "split", datasources = connections)
 
+# Grab potential errors that have been reported at each DataSHIELD server
+datashield.errors()
 
 # Logout from the OPAL server
-DSI::datashield.logout(connections)
+datashield.logout(connections)
 
 # Clean up
 rm(list = ls())
